4594d425e9af117991c3f5b72082f57a281f4bc3
Wadrian Macario <wadrianmacario4@gmail.com>
2026-04-15 13:36:22 -0300

    Adicionadas paginas de login, cadastro e um script simples

---
+++ script.js
@@ -0,0 +1,43 @@
+//CADASTRO
+const form = document.getElementById('cadastro-form');
+
+if (cadastroForm) {
+    cadastroForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const user = document.getElementById("newUser").value;
+        const email = document.getElementById("newEmail").value;
+        const pass = document.getElementById("newPass").value;
+
+        const userData = {
+            username: user,
+            email: email,
+            password: pass
+        };
+
+        localStorage.setItem("userData", JSON.stringify(userData));
+
+        alert("Cadastro realizado!");
+        window.location.href = "login.html";
+    });
+}
+
+//LOGIN
+const loginForm = document.getElementById('login-form');
+
+ if (loginForm) {
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const email = document.getElementById('login-email').value;
+        const password = document.getElementById('login-password').value;
+
+        const userData = JSON.parse(localStorage.getItem("userData"));
+
+        if (email === userData.email && password === userData.password) {
+            alert('Logado com sucesso!');
+        } else {
+            alert('Email ou senha inválidos.');
+        }
+    });
+}
